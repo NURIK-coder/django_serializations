79576ff75a4e78293e372fc2cd124e298785e2be
nurilloh_coder <136317567+NURIK-coder@users.noreply.github.com>
2024-08-29 18:25:56 +0500

    change

--- my_app/serializes.py
+++ my_app/serializes.py
@@ -10,6 +10,8 @@ from my_app.models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
     # description = serializers.CharField(write_only=True) #переопределение Когда serializers.Serializer
+
+
     class Meta:
         model = Note
         fields = ['id', 'title', 'description', 'created_at']
